ff17843dd6a9db7d01d19e1d87d2a19547d4aa13
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-05-15 18:01:14 +0900

    mathjax works

--- BuildDocHTML.sh
+++ BuildDocHTML.sh
@@ -10,7 +10,7 @@
 
 #### Convert knitted PDF file to html with pandoc ####
 cd /git_repositories/simPHDoc/PDF
-pandoc simPH_vignette.tex -o simPHDoc.html --bibliography HRBibliography.bib --bibliography HRPackages.bib
+pandoc simPH_vignette.tex -o simPHDoc.html --mathjax --bibliography HRBibliography.bib --bibliography HRPackages.bib
 
 # Clean up
 
@@ -23,16 +23,26 @@ cat simDocHeader.htm /git_repositories/simPHDoc/PDF/simPHDoc.html simDocFooter.h
 cd /git_repositories/simPH
 
 # Commit changes and change to gh-pages branch
-git add .
-git commit -am "Committed by bash"
-git checkout gh-pages
+Branch=$(git branch)
 
-# Move files
-mv -i /git_repositories/simPHDoc/HTML/fullDoc.html /git_repositories/simPH
+if [ "$Branch" = "* gh-pages" ]
+then
+	# Move files
+	mv -i /git_repositories/simPHDoc/HTML/fullDoc.html /git_repositories/simPH
+	rm /git_repositories/simPHDoc/HTML/fullDoc.html
 
-cp -ir /git_repositories/simPHDoc/PDF/figure /git_repositories/simPH
+	cp -ir /git_repositories/simPHDoc/PDF/figure /git_repositories/simPH
+else
+	git branch 
+	git add .
+	git commit -am "Committed by bash"
+	git checkout gh-pages
 
+	# Move files
+	mv -i /git_repositories/simPHDoc/HTML/fullDoc.html /git_repositories/simPH
 
+	cp -ir /git_repositories/simPHDoc/PDF/figure /git_repositories/simPH
+fi
 
 
 ######## Some manual clean up required ###############
