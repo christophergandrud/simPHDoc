9ad986c274cd625d97ef534aceeab83588e5494f
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-05-15 12:01:51 +0900

    working script

--- BuildDoc.sh
+++ BuildDoc.sh
@@ -9,8 +9,15 @@
 
 
 #### Convert knitted PDF file to html with pandoc ####
-cd /git_repositories/simPHDoc/HTML
+cd /git_repositories/simPHDoc/PDF
 pandoc simPH_vignette.tex -o simPHDoc.html --bibliography HRBibliography.bib --bibliography HRPackages.bib
+
+# Clean up
+
+
+# Combine with header and footer
+cd /git_repositories/simPHDoc/HTML
+cat simDocHeader.htm /git_repositories/simPHDoc/PDF/simPHDoc.html simDocFooter.htm >> fullDoc.html
 
 #### Move files to the gh-pages branch of simPH ####
 cd /git_repositories/simPH
@@ -21,6 +28,9 @@ git commit -am "Committed by bash"
 git checkout gh-pages
 
 # Move files
-mv -i /git_repositories/simPHDoc/HTML/simPHDoc.html /git_repositories/simPH
+mv -i /git_repositories/simPHDoc/HTML/fullDoc.html /git_repositories/simPH
 
-cp -i /git_repositories/simPHDoc/PDF/figure /git_repositories/simPH
+cp -ir /git_repositories/simPHDoc/PDF/figure /git_repositories/simPH
+
+
+######## Some manual clean up required ###############
